381c3a73f18520dd334de1a0202e6e588e6ce54d
Nathan Burns <nate10nate@gmail.com>
2025-10-31 12:24:55 -0400

    Configured Clerk provider with environment variables

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,17 +5,19 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClerk } from '@jsrob/ngx-clerk';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 
-// Nathan TODO: set up environment variable for Clerk publishable key when server is ready
-const CLERK_PUBLISHABLE_KEY = '';
+const CLERK_PUBLISHABLE_KEY = environment.clerkPublishableKey;
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideClerk({
       publishableKey: CLERK_PUBLISHABLE_KEY,
       signInUrl: '/login',
