44d5f45d5c437d976f8ad074340344375a451b2a
Nathan Burns <nate10nate@gmail.com>
2025-11-08 10:23:58 -0500

    Add additional notification settings to IAccountSettings interface

--- frontend/src/app/features/user-profile/models/account-settings.ts
+++ frontend/src/app/features/user-profile/models/account-settings.ts
@@ -2,4 +2,8 @@ export interface IAccountSettings {
   firstName: string;
   lastName: string;
   email: string;
+  appliedForVerification: boolean;
+  enableNearbyEventNotifications: boolean;
+  enableFavoriteTeamNotifications: boolean;
+  enableSafetyAlertNotifications: boolean;
 }
