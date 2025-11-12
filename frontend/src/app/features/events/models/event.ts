cd24d43f07bf020634249e27b3df0e75084fc8bf
Nathan Burns <nate10nate@gmail.com>
2025-11-12 16:38:58 -0500

    Refactor IEvent interface to include detailed event properties and types

--- frontend/src/app/features/events/models/event.ts
+++ frontend/src/app/features/events/models/event.ts
@@ -1,1 +1,18 @@
-export interface IEvent {}
+import { EventTypeEnum } from '../../../shared/models/event-type-enum';
+import { LeagueEnum } from '../../../shared/models/league-enum';
+
+export interface IEvent {
+  eventID: string; // UUID
+  eventType: EventTypeEnum;
+  eventName: string;
+  dateTime: string; // ISO 8601 format
+  location: string;
+  imageUrl?: string; // optional URL to an image representing the event
+  teamLogos?: {
+    home?: string; // optional URL to home team logo
+    away?: string; // optional URL to away team logo
+  }; // optional array of team logo URLs associated with the event
+  league?: LeagueEnum;
+  isUserCreated?: boolean; // indicates if the event was created by the user
+  isSaved: boolean; // indicates if the event is saved by the user
+}
