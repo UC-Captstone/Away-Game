8279ce6a5a5972d0f7ab3ddade02663375ff3055
Rudi <vogelrj@mail.uc.edu>
2025-11-14 22:37:50 -0500

    added teams and user favorite routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from app.db.session import init_db
-from app.routes import games, users
+from app.routes import games, users, teams, user_favorite_teams, favorites
 
 app = FastAPI(title="Away-Game API")
 
 app.include_router(games.router)
 app.include_router(users.router)
+app.include_router(teams.router)
+app.include_router(user_favorite_teams.router)
+app.include_router(favorites.router)
 
 @app.on_event("startup")
 async def _startup() -> None:
