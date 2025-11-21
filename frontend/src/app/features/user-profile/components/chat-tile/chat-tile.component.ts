9fda984b743498a55ae7fd0dac47d066aed8981e
Nathan Burns <nate10nate@gmail.com>
2025-11-21 09:05:58 -0500

    Remove unused EventEmitter import from ChatTileComponent for cleaner code

--- frontend/src/app/features/user-profile/components/chat-tile/chat-tile.component.ts
+++ frontend/src/app/features/user-profile/components/chat-tile/chat-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IChatMessage } from '../../../community/models/chat-message';
 
