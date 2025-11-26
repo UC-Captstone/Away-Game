280648785d33cbfa6d1f9e0db56335b92a57023f
Nathan Burns <nate10nate@gmail.com>
2025-11-25 22:01:45 -0500

    add event model interface for event management

---
+++ frontend/src/app/features/events/models/event.ts
@@ -0,0 +1,18 @@
+import { EventTypeEnum } from '../../../shared/models/event-type-enum';
+import { LeagueEnum } from '../../../shared/models/league-enum';
+
+export interface IEvent {
+  eventID: string; // UUID
+  eventType: EventTypeEnum;
+  eventName: string;
+  dateTime: Date;
+  location: string;
+  imageUrl?: string; // optional URL to an image representing the event
+  teamLogos?: {
+    home?: string; // optional URL to home team logo
+    away?: string; // optional URL to away team logo
+  }; // optional array of team logo URLs associated with the event
+  league?: LeagueEnum;
+  isUserCreated?: boolean; // indicates if the event was created by the user
+  isSaved: boolean; // indicates if the event is saved by the user
+}
