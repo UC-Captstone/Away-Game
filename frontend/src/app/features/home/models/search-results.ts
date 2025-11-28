4a97366a7d3967d7e40a8d7fe6f18db9792a7d52
Nathan Burns <nate10nate@gmail.com>
2025-11-28 13:04:58 -0500

    add ISearchResults interface for search results model

---
+++ frontend/src/app/features/home/models/search-results.ts
@@ -0,0 +1,13 @@
+import { SearchTypeEnum } from './search-type-enum';
+
+export interface ISearchResults {
+  id: string; // teamID, gameID, eventID, etc.
+  type: SearchTypeEnum;
+  title: string; // Name of the team, game, event, etc.
+  imageUrl?: string; // Optional image URL for the result
+  teamLogos?: {
+    homeLogo?: string;
+    awayLogo?: string;
+  };
+  metadata?: any; // Additional metadata relevant to the search result
+}
