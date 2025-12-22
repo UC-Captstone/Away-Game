4dcf0332f22cfd21cd78baf8b29003bebc6e597a
Nathan Burns <nate10nate@gmail.com>
2025-12-21 22:40:39 -0500

    Refactor main.py to include api_router and streamline route management

--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
-from app.routes import auth, games, users
+from app.routes import auth, games, users, teams, user_favorite_teams, favorites, profile
 
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(games.router)
 api_router.include_router(users.router)
+api_router.include_router(teams.router)
+api_router.include_router(user_favorite_teams.router)
+api_router.include_router(favorites.router)
+api_router.include_router(profile.router)
