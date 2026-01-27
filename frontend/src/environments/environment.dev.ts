84a8a4bfd604003e8ba255020d57c421f8d41790
ahart21 <aidenhartranft@gmail.com>
2026-01-26 19:18:51 -0500

    fix routes

---
+++ frontend/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://awaygamefunctionapp-dev-b6d0bpbmfyb4bre9.canadacentral-01.azurewebsites.net/api',
+  clerkPublishableKey: 'pk_test_Y29taWMtbGFkeWJ1Zy03My5jbGVyay5hY2NvdW50cy5kZXYk',
+};
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://prod-url.com',
+  apiUrl: 'https://awaygamefunctionapp-prd-bjftgydhara3d4a6.canadacentral-01.azurewebsites.net/api',
   //clerkPublishableKey: 'prod-publishable-key', //Nathan: replace later
 };
