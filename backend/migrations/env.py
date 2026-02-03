65bc985c49fc30b9509f1105627b677229dbbef4
ahart21 <aidenhartranft@gmail.com>
2026-02-03 18:38:21 -0500

    get local DB up and running

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+app_dir = os.path.join(backend_dir, "app")
+sys.path.insert(0, app_dir)
 sys.path.insert(0, backend_dir)
 
 config = context.config
