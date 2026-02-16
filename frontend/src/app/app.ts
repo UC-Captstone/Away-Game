b1533c6bf5bb061d5fe752831434d0990e686966
Nathan Burns <nate10nate@gmail.com>
2026-02-16 08:09:24 -0500

    Add navbar component and integrate loading state management for improved navigation experience

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './shared/components/navbar/navbar.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  showNavbar: WritableSignal<boolean> = signal(true);
+  isLoading: WritableSignal<boolean> = signal(false);
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const hiddenRoutes = ['/login', '/signup', '/profile', '/login#/factor-one'];
+        this.showNavbar.set(!hiddenRoutes.includes(event.urlAfterRedirects));
+      });
+  }
+
+  onNavBarLoading(value: boolean): void {
+    this.isLoading.set(value);
+  }
 }
