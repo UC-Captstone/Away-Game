e2fb026bfd5e90a646fd536720e7a2a9fa489f24
Nathan Burns <nate10nate@gmail.com>
2026-04-13 16:02:41 -0400

    Update map header and page components to support place category filters and enhance nearby places display

--- frontend/src/app/features/map/components/map-header/map-header.component.ts
+++ frontend/src/app/features/map/components/map-header/map-header.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IPlaceCategoryFilters, PlaceCategory } from '../../../../shared/models/place-category';
 
 @Component({
   selector: 'app-map-header',
@@ -11,12 +12,16 @@ export class MapHeaderComponent {
   @Input() showGameOverlay: boolean = true;
   @Input() showEventOverlay: boolean = true;
   @Input() showSafetyOverlay: boolean = true;
-  @Input() showUserMarker: boolean = true;
+  @Input() placeCategoryFilters: IPlaceCategoryFilters = {
+    restaurant: true,
+    bar: true,
+    hotel: true,
+  };
 
   @Output() toggleGameOverlay = new EventEmitter<void>();
   @Output() toggleEventOverlay = new EventEmitter<void>();
   @Output() toggleSafetyOverlay = new EventEmitter<void>();
-  @Output() toggleUserMarker = new EventEmitter<void>();
+  @Output() togglePlaceCategory = new EventEmitter<PlaceCategory>();
 
   onToggleGameOverlay(): void {
     this.toggleGameOverlay.emit();
@@ -30,7 +35,7 @@ export class MapHeaderComponent {
     this.toggleSafetyOverlay.emit();
   }
 
-  onToggleUserMarker(): void {
-    this.toggleUserMarker.emit();
+  onTogglePlaceCategory(category: PlaceCategory): void {
+    this.togglePlaceCategory.emit(category);
   }
 }
